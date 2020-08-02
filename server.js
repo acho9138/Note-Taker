03859cc5b339f3ce28aab6138628c69ae08f85c7
Anna C <anna.s.chong@gmail.com>
2020-08-02 19:51:56 +1000

    ADD get and post requests to display and post notes to db.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = 8080;
@@ -16,6 +17,40 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(publicPath, "notes.html"));
 });
+
+app.get("/api/notes", (req, res) => {
+    return res.json(getData());
+});
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    newNote['id'] = uuidv4()
+
+    let currentNotes = getData()
+    currentNotes.push(newNote)
+    setData(currentNotes);
+
+    return res.status(201).end();
+});
+
+const getData = () => {
+    try {
+        const jsonString = fs.readFileSync("./db/db.json")
+        const notes = JSON.parse(jsonString)
+        return notes
+    } catch(err) {
+        throw (err)
+    }
+}
+
+const setData = (data) => {
+    try {
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), 'utf8')
+        console.log("Successfully added the note");
+    } catch(err) {
+        throw (err)
+    }
+}
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
